df51af0998e3909575ecf0ea2c7119907450324d
lainodHO <133787146+lainodHO@users.noreply.github.com>
2024-03-06 09:42:17 -0600

    Actualizacion
    Corregir errores

--- No-Bloqueante.js
+++ No-Bloqueante.js
@@ -4,13 +4,13 @@ setTimeout(function () {
     console.log('Tarea 1');}, 3000);//Espera 3 segundos
 
 setTimeout(function () { 
-    console.log('Tarea 1');}, 3000);//Espera 3 segundos
+    console.log('Tarea 2');}, 3000);//Espera 3 segundos
 
 setTimeout(function () { 
-    console.log('Tarea 1');}, 3000);//Espera 3 segundos
+    console.log('Tarea 3');}, 3000);//Espera 3 segundos
 
 setTimeout(function () { 
-    console.log('Tarea 1');}, 3000);//Espera 3 segundos
+    console.log('Tarea 4');}, 3000);//Espera 3 segundos
 
     
 console.log('Tarea 5');
