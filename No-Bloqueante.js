09954cb90e0da744c3350b373f2e3a12f790c2d6
lainodHO <133787146+lainodHO@users.noreply.github.com>
2024-03-06 09:37:58 -0600

    Js No-Bloqueante
    Es un script de tarea 5

---
+++ No-Bloqueante.js
@@ -0,0 +1,18 @@
+'use strict'
+
+setTimeout(function () { 
+    console.log('Tarea 1');}, 3000);//Espera 3 segundos
+
+setTimeout(function () { 
+    console.log('Tarea 1');}, 3000);//Espera 3 segundos
+
+setTimeout(function () { 
+    console.log('Tarea 1');}, 3000);//Espera 3 segundos
+
+setTimeout(function () { 
+    console.log('Tarea 1');}, 3000);//Espera 3 segundos
+
+    
+console.log('Tarea 5');
+console.log(process.uptime());
+console.log('Fin del Programa');
